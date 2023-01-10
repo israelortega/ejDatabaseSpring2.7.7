a32aecac1b54eb8726f0226429deba4377a5e5a3
israelortega <israelortegac@gmail.com>
2023-01-10 13:27:31 -0600

    update(UsuarioRepo): Se actualiza repositorio

---
+++ src/main/java/mx/unam/sa/ejDatabase/repository/UsuarioRepo.java
@@ -0,0 +1,17 @@
+/*
+ * (c) UNAM, 2022
+ */
+package mx.unam.sa.ejDatabase.repository;
+
+
+import mx.unam.sa.ejDatabase.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author israel1971
+ */
+@Repository
+public interface UsuarioRepo extends JpaRepository<Usuario,Long> {
+}
