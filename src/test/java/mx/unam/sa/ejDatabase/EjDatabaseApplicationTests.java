748674fde46da80fbd77ed2c13c937bb45cc80a6
Israel <iortega@dgp.unam.mx>
2023-01-10 13:06:53 -0600

    starting: se inicia el proyecto

---
+++ src/test/java/mx/unam/sa/ejDatabase/EjDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.sa.ejDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
